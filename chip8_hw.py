118d2d286498ba5ef4c89ed583157a6b7938f45c
michael-leroy <michael.d.leroy@gmail.com>
2015-01-13 19:12:47 -0800

    more instructions added, still work in progress

--- chip8_hw.py
+++ chip8_hw.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import random
 
 class chip8(object):
     def __init__(self):
@@ -90,26 +91,126 @@ class chip8(object):
 	        self.pc += 2
 	elif (opcode & 0xF000) == 0x5000:
 	    #0x5000 = 5xy0 SE, skip next instruction if Vx = Vy
-	    if self.V[(opcode & 0x0F00) <<  8] == self.V[(opcode & 0x00F0) >> 4]:
+	    if self.V[(opcode & 0x0F00) >>  8] == self.V[(opcode & 0x00F0)] >> 4]:
 		self.pc += 4
 	    else:
 		self.pc += 2
 	elif (opcode & 0xF000) == 0x6000:
 	    #0x6000 = 6xkk put value kk in to register Vx
-	    self.V[(opcode & 0x0F00) << 8] = opcode & 0x00FF
+	    self.V[(opcode & 0x0F00) >> 8] = opcode & 0x00FF
 	    self.pc += 2
 	elif (opcode & 0xF000) == 0x7000:
 	    #0x7000 = 7xkk Add Vx. Add value kk to register Vx and store value
 	    #in Vx
 	    self.V[(opcode & 0x0F00) >> 8] += opcode & 0x00FF 
 	    self.pc += 2
-	elif (opcode & 0xF000) == 0x8000:
-	    #0x8000 = 8x70 Set Vx = Vy
-	    #TODO: The several 0x8000 instructions.
+	elif (opcode & 0xF000) == 0x8000 and (opcode & 0x000F) == 0x0:
+	    #0x8000 = 8xy0 Set Vx = Vy
+            self.V[(opcode & 0x0F00) >>  8] = selv.V[(opcode & 0x00F0) >>  4 ]
+            self.pc += 2
+        elif (opcode & 0xF000) == 0x8000 and (opcode & 0x000F) == 0x1:
+            #8xy1 Set Vx = Vx or Vy
+            self.V[(opcode & 0x0F00) >>  8] = self.V[(opcode & 0x0F00) >>  8] | selv.V[(opcode & 0x00F0)[ >>  4]
+            self.pc += 2
+        elif (opcode & 0xF000) == 0x8000 and (opcode & 0x000F) == 0x2:
+            #8xy2 Set Vx = Vx and Vy
+            self.V[(opcode & 0x0F00) >>  8] = self.V[(opcode & 0x0F00) >>  8] & selv.V[(opcode & 0x00F0)] >>  4]
+            self.pc += 2
+        elif (opcode & 0xF000) == 0x8000 and (opcode & 0x000F) == 0x3:
+            #8xy3 Set Vx = Vx XOR Vy
+            self.V[(opcode & 0x0F00) >>  8] = self.V[(opcode & 0x0F00) >>  8] ^ selv.V[(opcode & 0x00F0)] >>  4]
+            self.pc += 2
+        elif (opcode & 0xF000) == 0x8000 and (opcode & 0x000F) == 0x4:
+            #8xy4 Set Vx = Vx + Vy set VF = carry is over 255
+            #if the two value are over 8 bit (255) carry the result in VF
+            if (self.V[(opcode & 0x0F00) >>  8] + selv.V[(opcode & 0x00F0) >>  4]) > 0xFF:
+                self.V[0xF] = 1
+                self.V[(opcode & 0x0F00) >>  8] += (selv.V[(opcode & 0x00F0) >> 4]) & 0xFF)
+            else:
+                self.V[(opcode & 0x0F00) >>  8] += selv.V[(opcode & 0x00F0) >> 4]
+                self.V[0xF] = 0
+            self.pc += 2
+        elif (opcode & 0xF000) == 0x8000 and (opcode & 0x000F) == 0x5:
+            #8xy5 - Sub Vx, Vy.
+            #Set Vx = Vx - Vy, set Vf = if it does NOT borrow
+            if (self.V[(opcode & 0x0F00) >>  8] > selv.V[(opcode & 0x00F0) >>  4]):
+                self.V[0xF] = 1 #NOT BORROW
+            else:
+                self.V[0xF] = 0 #Brrow
+            self.V[(opcode & 0x0F00) >>  8] -= selv.V[(opcode & 0x00F0) >> 4]
+            self.pc += 2
+        elif (opcode & 0xF000) == 0x8000 and (opcode & 0x000F) == 0x6:
+            #8xy6 - SHR Vx {, Vy}
+            #If the least significant bit of Vx is 1, set VF = 1
+            #otherwise 0. Then divide Vx by 2
+            if self.V[(opcode & 0x0F00) >>  8] & 0b00000001 == 0b1:
+                self.V[0xF] = 1
+            else:
+                self.V[0xF] = 0
+            self.V[(opcode & 0x0F00) >>  8] /= 2
+            self.pc += 2
+        elif (opcode & 0xF000) == 0x8000 and (opcode & 0x000F) == 0x7:
+            #Set Vx = Vy - Vx, set VF = NOT BORROW
+            if selv.V[(opcode & 0x00F0) >> 4] > self.V[(opcode & 0x0F00) >>  8]:
+                self.V[0xF] = 1 #NOT BOROW
+            else:
+                self.V[0xF] = 0
+            self.V[(opcode & 0x0F00) >>  8] = selv.V[(opcode & 0x00F0) >> 4] - self.V[(opcode & 0x0F00) >>  8]
+            self.pc += 2
+        elif (opcode & 0xF000) == 0x8000 and (opcode & 0x000F) == 0xE:
+            #8xyE - SHL Vx {, Vy}
+            if (self.V[(opcode & 0x0F00) >>  8] >> 7) == 1:
+                self.V[0xF] = 1
+            else:
+                self.V[0xF] = 0
+            self.V[(opcode & 0x0F00) >>  8] = self.V[(opcode & 0x0F00) >>  8] << 1
+            self.pc += 2
+        elif (opcode & 0xF000) == 0x9000:
+            #9xy0 - SNE Vx, Vy
+            #Skip next instruction if Vx != Vy
+            if self.V[(opcode & 0x0F00) >>  8] != selv.V[(opcode & 0x00F0) >> 4]:
+                self.pc += 4
+            else:
+                self.pc += 2
         elif (opcode & 0xF000) == 0xA000:
             #0xA000 == ANNN: Sets I to the address NNN
             self.I = opcode & 0x0FFF
             self.pc += 2
+        elif (opcode & 0xF000) == 0xB000:
+            #Bnnn - Jp V0, addr
+            self.pc = (opcode & 0x0FFF) + self.V[0x0]
+        elif (opcode & 0xF000) == 0xC000:
+            #Cxkk - RND Vx, byte
+            #Gen number between 0x0 and 0xFF and & it with the value of kk then
+            #store in Vx
+            self.V[(opcode & 0x0F00) >>  8] = random.randrange(0x0, 0xFF) & (opcode & 0x00FF)
+            self.pc += 2
+        elif (opcode & 0xF000) == 0xD000:
+            # DRW Vx, Vy. nibble
+            drw_x = self.V[(opcode & 0x0F00) >>  8]
+            drw_y = selv.V[(opcode & 0x00F0) >> 4]
+            drw_height = opcode & 0x000F
+            sprite_data = []
+            #set VF to zero because there has been no collision yet
+            self.V[0xF] = 0
+            #Read the sprite data from memory
+            for x in xrange(drw_height):
+                sprite_data.append(self.memory[self.I + x])
+            for sprite_row in sprite_data:
+                #Sprites are always 8 pixels wide
+                for pixel_offset in xrange(8):
+                    location = drw_x + pixel_offset + ((drw_y + sprite_row) * 64)
+                    if (drw_y + sprite_row
+                    drw_mask = 1 << 8 - pixel_offset
+                    curr_pixel = (sprite_row & mask) >> (8 - pixel_offset)
+                    self.gfx[location] ^= curr_pixel
+                    if self.gfx[location] == 0:
+                        self.V[0xF] = 1
+                    else:
+                        self.V[0xF] = 0
+
+
+
         else:
             print('Unknown/Invalid opcode ' + str(opcode))
 
